fe14c4031b5ef772e3eb0ee40fee71d8588e76dc
Stepan Snigirev <snigirev.stepan@gmail.com>
2019-05-22 22:15:10 +0200

    driver

---
+++ touchpad/touchpad.h
@@ -0,0 +1,41 @@
+/**
+ * @file indev.h
+ *
+ */
+
+#ifndef INDEV_H
+#define INDEV_H
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include <stdbool.h>
+#include <stdint.h>
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void touchpad_init(void);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+/**********************
+ *      MACROS
+ **********************/
+
+#endif
